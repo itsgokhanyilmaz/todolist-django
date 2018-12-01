f1821ffa97771e9a50f53ebd8005bb96f217a38e
GokhanYilmaz44 <yilmazgokhan.1992@gmail.com>
2018-12-02 01:27:56 +0300

    added django swagger

--- todolist/todolist/settings.py
+++ todolist/todolist/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'todos',
     'rest_framework',
+    'rest_framework_swagger',
 ]
 
 MIDDLEWARE = [
--- todolist/todolist/urls.py
+++ todolist/todolist/urls.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_swagger.views import get_swagger_view
+
+
+schema_view = get_swagger_view(title='TODOLIST API')
 
 urlpatterns = [
+    path('swagger/', schema_view),
     path('admin/', admin.site.urls),
     path('api/', include('todos.urls')),
 ]
